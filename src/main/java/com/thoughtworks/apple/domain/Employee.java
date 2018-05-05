a2162cb315eb7db700a2f7d53f1278b243078db4
QiYifei <qyf404@gmail.com>
2018-05-05 11:29:21 +0800

    feat(employee): add domain model

---
+++ src/main/java/com/thoughtworks/apple/domain/Employee.java
@@ -0,0 +1,56 @@
+package com.thoughtworks.apple.domain;
+
+import java.util.Objects;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private int age;
+    private String gender;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
